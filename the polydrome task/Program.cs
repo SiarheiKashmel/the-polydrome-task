8f7ca68c7794164b73832557cd762435a5de0b65
Сергей Кашмель <107143266+SiarheiKashmel@users.noreply.github.com>
2022-07-26 18:11:31 +0400

    Добавьте файлы проекта.

---
+++ the polydrome task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = { 'q', 'w', 'e', 'w', 'q' };
+            if (IsPalindrom(arr))
+                Console.WriteLine("Палиндром");
+            else
+                Console.WriteLine("Не палиндром");
+        }
+
+        static bool IsPalindrom(char[] word)
+        {
+            for (int i = 0; i < word.Length / 2; i++)
+                if (word[i] != word[word.Length - 1 - i])
+                    return false;
+            return true;
+        }
+    }
